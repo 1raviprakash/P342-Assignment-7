fbaf1440aa3c9ff3317e5be8c0d15e3f1eea344d
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2020-12-13 22:11:22 +0530

    Update My_lib.py

--- My_lib.py
+++ My_lib.py
@@ -28,7 +28,7 @@ def factorial(num):
     elif num == 0:
         return 1
     else:
-        for i in range(1, num + 1):
+        for i in range(1, num + 1): 
             fact = fact * i
         return fact
 
